ba65a5ca47500ce581f190b5e6e20549d76e02f4
josearc <juanjose.arcani2309@gmail.com>
2021-06-15 21:08:29 -0400

    Implementando PUT, DELETE, GET y POST en express

--- routes/movies.js
+++ routes/movies.js
@@ -6,19 +6,70 @@ function moviesApi(app) {
   const router = express.Router();
   app.use('/api/movies', router);
 
+  // obtener todo ***Read***
   router.get('/', async function (req, res, next) {
     try {
       const movies = await Promise.resolve(moviesMock);
-
       res.status(200).json({
         data: movies,
         message: 'movies listed'
       });
-
     } catch (err) {
       next(err);
     }
-  })
+  });
+
+  // obtener película específica ***Read***
+  router.get('/:movieId', async function (req, res, next) {
+    try {
+      const movies = await Promise.resolve(moviesMock[0]);
+      res.status(200).json({
+        data: movies,
+        message: 'movie retrieved'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  // creación de las películas, regresamos el Id de la película ***Create***
+  router.post('/', async function (req, res, next) {
+    try {
+      const createdMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(201).json({
+        data: createdMovieId,
+        message: 'movie created'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  // actualización de una película ***Update***
+  router.put('/:movieId', async function (req, res, next) {
+    try {
+      const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: updatedMovieId,
+        message: 'movie updated'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  // borrar una película ***Delete***
+  router.delete('/:movieId', async function (req, res, next) {
+    try {
+      const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: deletedMovieId,
+        message: 'movie deleted'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 }
 
 module.exports = moviesApi;
