1f3519ca73a428282c6713c0c22a6a66091061b6
josearc <juanjose.arcani2309@gmail.com>
2021-06-30 17:32:37 -0400

    Creación de test para utilidades TDD - clase 39

---
+++ test/utils.buildMessage.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const buildMessage = require('../utils/buildMessage');
+
+// cuando mandamos cierto mensaje recibimos cierta respuesta
+// solo este test .only
+describe.only('utils - buildMessage', function () {
+  describe('when receives an entity and an action', function () {
+    it('should return the respective message', function () {
+      const result = buildMessage('movie', 'create');
+      const expected = 'movie created';
+      assert.strictEqual(result, expected);
+    })
+  })
+
+  describe('when receives an entity and an action abd us a list', function () {
+    it('should return the respective message with the entity in plural', function () {
+      const result = buildMessage('movie', 'list');
+      const expected = 'movies listed';
+      assert.strictEqual(result, expected);
+    })
+  })
+})
