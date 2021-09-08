0b3741bfdb7bbb80e7dabe43dfef36f98aba8988
josearc <juanjose.arcani2309@gmail.com>
2021-09-07 20:05:56 -0400

    internal server error fixed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 const { config } = require('./config');
 const moviesApi = require('./routes/movies.js');
+const userMoviesApi = require('./routes/userMovies');
+const authApi = require('./routes/auth');
 
 const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
 const notFoundHandler = require('./utils/middleware/notFoundHandler');
@@ -10,9 +13,12 @@ const notFoundHandler = require('./utils/middleware/notFoundHandler');
 const debug = require('debug')('app:server');
 // middleware body parser
 app.use(express.json());
+app.use(helmet());
 
 // routes
 moviesApi(app);
+userMoviesApi(app);
+authApi(app);
 
 // catch 404
 app.use(notFoundHandler);
@@ -26,5 +32,6 @@ app.use(errorHandler);
 debug('hello debug');
 
 app.listen(config.port, function () {
+  // eslint-disable-next-line no-console
   console.log(`Listening http://localhost:${config.port}`);
 })
